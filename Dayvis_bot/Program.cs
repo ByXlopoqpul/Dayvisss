3c817ae04b5cfd335dcd4bd680fc5eaad599ed2b
ByXlopoqpul <byxlopoful@gmail.com>
2026-02-19 18:21:58 +0600

    Add files via upload

---
+++ Dayvis_bot/Program.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot;
+
+partial class Program
+{
+    static async Task Main(string[] args)
+    {
+        var bot = new TelegramBotClient("8344813594:AAG4-K41gYmMfvXknZkBCTkApCyHc1kxqXc");
+        var me = await bot.GetMe();
+        Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+    }
+}
