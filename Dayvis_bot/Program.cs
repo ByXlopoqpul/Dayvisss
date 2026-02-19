8ea823a1a2f6186edbf6995af9bbd421ed6ff10e
ByXlopoqpul <byxlopoful@gmail.com>
2026-02-19 18:24:05 +0600

    Update Program.cs

--- Dayvis_bot/Program.cs
+++ Dayvis_bot/Program.cs
@@ -4,8 +4,9 @@ partial class Program
 {
     static async Task Main(string[] args)
     {
-        var bot = new TelegramBotClient("8344813594:AAG4-K41gYmMfvXknZkBCTkApCyHc1kxqXc");
+        var bot = new TelegramBotClient("BOT_TOKEN");
         var me = await bot.GetMe();
         Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
     }
+
 }
